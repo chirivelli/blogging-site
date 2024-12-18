547e543052088a1b222aa668b5560b2c24051058
Sathwik <sathwikchirivelli@gmail.com>
2024-12-18 19:20:52 +0530

    Debugging using shadow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 const App = () => {
 
     return (
-        <div className='container flex justify-around'>
-            <nav>
-                <a href='#'>About me</a>
-                <a href='#'>Projects</a>
-                <a href='#'>Resume</a>
-            </nav>
+        <div className='container flex justify-around bg-red-600 p-3 mx-auto'>
+            <input type="checkbox"/>
+            <p>{Date.now()}</p>
+            <p>+$100</p>
+            <p className='bg-blue-400 px-3 py-1 rounded-xl'>tag</p>
+            <p>Bank Logo</p>
+            <p>isExcluded</p>
         </div>
     )
 }
